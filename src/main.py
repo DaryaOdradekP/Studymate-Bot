3c2e53f18dc005c051c5cf82ae1e010a8bd9c683
Darya Pavlova <dasha.pavlova289@gmail.com>
2026-08-20 14:42:51 +0500

    Add application logging

--- src/main.py
+++ src/main.py
@@ -4,8 +4,13 @@ from src.bot import bot, dp
 
 from src.database.init_db import init_db
 
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 async def main():
-    print("Bot is starting...")
+    logger.info("Bot is starting...")
     init_db()
     await dp.start_polling(bot)
 
